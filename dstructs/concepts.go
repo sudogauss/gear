5b40947ef3e5b7e6850df216da500a57c719671b
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2023-05-09 15:31:24 +0200

    Init project. Create VERSION file. Working on pairs extension

---
+++ dstructs/concepts.go
@@ -0,0 +1,5 @@
+package dstructs
+
+type Comparable interface {
+	Compare(interface{}) int
+}
---
+++ dstructs/pair.go
@@ -0,0 +1,26 @@
+package dstructs
+
+import "golang.org/x/exp/constraints"
+
+type Pair[K constraints.Ordered, V any] struct {
+	First  K
+	Second V
+}
+
+func New[K constraints.Ordered, V any](_First K, _Second V) *Pair[K, V] {
+	return &Pair[K, V]{
+		First:  _First,
+		Second: _Second,
+	}
+}
+
+func (p *Pair[K, V]) Compare(other *Pair[K, V]) int {
+
+	if p.First == other.First {
+		return 0
+	} else if p.First < other.First {
+		return -1
+	} else {
+		return 1
+	}
+}
